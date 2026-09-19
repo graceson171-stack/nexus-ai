570ee600179e9aefa6c60d08c7e8dcfc653e5e56
graceson171-stack <graceson171@gmail.com>
2026-09-19 15:25:07 +0530

    Learn Python Inputs

---
+++ src/input_tests.py
@@ -0,0 +1,7 @@
+name = input("What is your name?")
+age = int(input("What is your age?"))
+gpa = float(input("What is your GPA?"))
+
+print(f"Name : {name}")
+print(f"Age : {age}")
+print(f"GPA : {gpa}")
