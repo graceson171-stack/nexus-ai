221d32e1fe4d5c478c26a5aefd87da83cb4e5070
graceson171-stack <graceson171@gmail.com>
2026-09-19 14:07:52 +0530

    Add Python Fundamentals

---
+++ src/variables.py
@@ -0,0 +1,10 @@
+name = "NEXUS"
+version = 0.1
+status = "development"
+project = "AI knoledge and Learning Engine"
+
+print(f"Name: {name}")
+print(f"Version: {version}")
+print(f"Status: {status}")
+print(f"Project: {project}")
+
