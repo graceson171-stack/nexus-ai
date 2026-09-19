831433de188bd2bdf76a8ab4970e38f0df4a58ba
graceson171-stack <graceson171@gmail.com>
2026-09-19 15:34:42 +0530

    Learning conditional statements

---
+++ src/decisions.py
@@ -0,0 +1,12 @@
+gpa = float(input("Enter your GPA: "))
+
+if gpa >= 9.0:
+    print("Excellent")
+elif gpa >=8.0:
+    print("Very Good")
+elif(gpa >= 7.0):
+    print("Good")
+elif(gpa >= 6.0):
+    print("Okay")
+else:
+    print("Needs improvement")
